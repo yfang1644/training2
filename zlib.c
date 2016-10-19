efef8627800ee9e9096d0d47f3a20dcbb0f4526f
yfang1644 <yfang1644@gmail.com>
2016-10-19 22:08:08 +0800

    add a file zlib.c

---
+++ zlib.c
@@ -0,0 +1,31 @@
+/*
+ * ============================================================================
+ *
+ *       Filename:  zlib.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  10/19/2016 10:07:31 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Fang Yuan (yfang@nju.edu.cn)
+ *   Organization:  nju
+ *
+ * ============================================================================
+ */
+
+
+#include <stdlib.h>
+
+/* 
+* ===  FUNCTION  =============================================================
+*         Name:  main
+*  Description:  
+* ============================================================================
+*/
+int main (int argc, char *argv[])
+{
+ return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
